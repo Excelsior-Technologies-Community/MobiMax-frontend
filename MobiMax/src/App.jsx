6eadd655d3e118afaa0ea078ae355a7c16925199
aadi2435 <131764810+aadi2435@users.noreply.github.com>
2026-07-02 15:09:26 +0530

    Done

---
+++ MobiMax/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <h1 className="text-6xl text-red-500 font-bold">
+        Tailwind is finally working 🚀
+      </h1>
+    </div>
+  );
+}
